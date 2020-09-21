0bfff52edf62cf4a8c32a097d5cdfbeaa59a3d3b
lf <18073072@qq.com>
2020-09-21 10:30:29 +0800

    9/21

--- .idea/chapter1.py
+++ .idea/chapter1.py
@@ -1,1 +1,21 @@
-import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+my_font = font_manager.FontProperties(fname=r"/System/Library/Fonts/PingFang.ttc")
+import numpy as np
+from base_class import paint_linear as pl #自定义绘图类
+from base_class import *
+
+#一、test paint_class
+o=pl("cos and sin")
+# o([[0,2*np.pi,50],[-np.pi,np.pi,50]],[[np.sin,"sin(x)"],[np.cos,"cos(x)"]])
+
+#二、第二章第5节由离散信号恢复到连续信号（本质是插值拟合）,提供3中插值方式进行比较
+# if self.g_o=="sinc":
+# elif self.g_o=="triangle":
+# elif self.g_o=="square":
+o_d=discrete2continuity(xrange=[-20,20],num=100,x_fun=np.sin,g_fun="square")
+x_range=np.linspace(-20,20,200)
+y_fit=o_d(x_range)         #由离散插值出的拟合函数
+y_rel=list(np.sin(x_range))#真实函数
+o=pl()
+o([x_range,x_range],[[y_fit,"fit"],[y_rel,"sin"]],x_type="x_o_y_o")
